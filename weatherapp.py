0b85c45bab210845260f12c78a036304dc9e56f6
Mullah-X <mulatiderrick@gmail.com>
2024-08-20 21:14:49 +0300

    API removed

--- weatherapp.py
+++ weatherapp.py
@@ -3,31 +3,15 @@ import requests
 
 # Function to get weather data
 def get_weather():
-    api_key = 'your_api_key_here'  # Replace with your actual API key
-    city = 'Karen, KE'
-    base_url = 'http://api.openweathermap.org/data/2.5/weather?'
-    
-    complete_url = f"{base_url}appid={api_key}&q={city}&units=metric"
-    
-    response = requests.get(complete_url)
-    data = response.json()
-    
-    if data['cod'] != '404':
-        main = data['main']
-        weather = data['weather'][0]
-        
-        temperature = main['temp']
-        pressure = main['pressure']
-        humidity = main['humidity']
-        weather_description = weather['description']
-        
-        weather_info = f"Temperature: {temperature}°C\n"
-        weather_info += f"Pressure: {pressure} hPa\n"
-        weather_info += f"Humidity: {humidity}%\n"
-        weather_info += f"Description: {weather_description}"
+    city = 'Karen'
+    base_url = f'http://wttr.in/{city}?format=%C+%t+%h+%P'
+
+    response = requests.get(base_url)
+    if response.status_code == 200:
+        weather_info = response.text
     else:
         weather_info = "City Not Found!"
-    
+
     label.config(text=weather_info)
 
 # Creating the GUI window
